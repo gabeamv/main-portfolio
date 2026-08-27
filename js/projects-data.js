43f0c1fe1e76bffde5ca02cae36a7d77813e60ff
gabeamv <gabrielvillanueva11155@gmail.com>
2026-08-26 17:29:29 -0700

    added bio, picture, and project links

--- js/projects-data.js
+++ js/projects-data.js
@@ -2,10 +2,32 @@
 // to add/remove projects from the page.
 const projects = [
   {
-    title: "Example Project",
-    description: "A short description of what this project does and why you built it.",
-    demoUrl: "https://youtube.com/your-demo-link",
-    repoUrl: "https://github.com/gabeamv/your-repo",
-    tags: ["JavaScript", "HTML", "CSS"],
+    title: "Overloaded - The Fitness API",
+    description: "A RESTful workout-tracking backend, providing structured endpoints for managing exercises, workouts, and workout sets.",
+    demoUrl: "",
+    repoUrl: "https://github.com/gabeamv/overloaded-api",
+    tags: ["Go", "PostgreSQL", "sqlc", "goose", "JWT Auth"],
   },
+  {
+    title: "SecureNotes ",
+    description: "An end-to-end hybrid encryption application that allows users to securely send files and messages to other valid users, as well as securely store files locally.",
+    demoUrl: "",
+    repoUrl: "https://github.com/gabeamv/CSharpTrainingArc",
+    tags: ["C#", "ASP.NET Core Web API", "Entity Framework Core", "PostgreSQL", "WPF", "Hybrid RSA/AES-GCM", "CNG API"],
+  },
+  {
+    title: "ChatApp",
+    description: "A real-time client-server chat application that supports asynchronous client-server communication, message broadcasting, connection management, and responsive UI updates.",
+    demoUrl: "",
+    repoUrl: "https://github.com/gabeamv/ChatApp",
+    tags: ["C#", "WPF", "WebSockets", "Asynchronous Programming"],
+  },
+  {
+    title: "mOral - The Dental Hygiene Application",
+    description: "A mobile Android application that promotes dental hygiene for users by gamifying dental hygiene activities through fun game-like features which leverage a points and rewards system.",
+    demoUrl: "",
+    repoUrl: "https://github.com/ArthurO-CSULB/Dentistry-App",
+    tags: ["Kotlin", "Jetpack Compose", "Firebase"],
+  },
+
 ];
